7bfec33a4c690067ba0cad966f17e2324e8bf5c9
Jérémy FARNAULT <jeremy.farnault@cirso.fr>
2016-11-16 14:42:29 +0100

    Adding zoom on portfolio images

--- js/scripts.js
+++ js/scripts.js
@@ -61,6 +61,58 @@ $(document).ready(function(){
     });
 });
 
+// Modal images portfolio
+$(document).ready(function(){
+
+    loadGallery(true, '.icon-zoom-portfolio span');
+
+    function disableButtons(counter_max, counter_current){
+        $('#show-previous-image, #show-next-image').show();
+        if(counter_max == counter_current){
+            $('#show-next-image').hide();
+        } else if (counter_current == 1){
+            $('#show-previous-image').hide();
+        }
+    }
+
+    function loadGallery(setIDs, setClickAttr){
+        var current_image,
+            selector,
+            counter = 0;
+
+        $('#show-next-image, #show-previous-image').click(function(){
+            if($(this).attr('id') == 'show-previous-image'){
+                current_image--;
+            } else {
+                current_image++;
+            }
+
+            selector = $('[data-image-id="' + current_image + '"]');
+            updateGallery(selector);
+        });
+
+        function updateGallery(selector) {
+            var $sel = selector;
+            current_image = $sel.data('image-id');
+            $('#image-gallery-caption').text($sel.data('caption'));
+            $('#image-gallery-title').text($sel.data('title'));
+            $('#image-gallery-image').attr('src', $sel.data('image'));
+            disableButtons(counter, $sel.data('image-id'));
+        }
+
+        if(setIDs == true){
+            $('[data-image-id]').each(function(){
+                counter++;
+                $(this).attr('data-image-id',counter);
+            });
+        }
+
+        $(setClickAttr).on('click',function(){
+            updateGallery($(this));
+        });
+    }
+});
+
 // Google maps
 var myCenter = new google.maps.LatLng(43.907154, 1.688826);
 var awayCenter = new google.maps.LatLng(-36.874670, 174.744214);
